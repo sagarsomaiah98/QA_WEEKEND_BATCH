0701e60c283a63878d5f5a93ff9c9e27eb74a011
Som <somaiah1988@gmail.com>
2023-02-11 12:03:01 -0500

    11

---
+++ QA_CLASS_11_FEB/src/Basics/HelloWorld.java
@@ -0,0 +1,21 @@
+package Basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Welcome to      QA Training");
+		
+		System.out.println("Welcome");
+		System.out.println("JAVA");
+		System.out.println("Selenium");
+		System.out.println("QA Session");
+		
+		
+		
+
+		
+	}
+
+}
---
+++ QA_CLASS_11_FEB/src/Basics/PrintvsPrintln.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class PrintvsPrintln {
+
+	public static void main(String[] args) {
+	
+	    System.out.println("Welcome to QA Training");
+		System.out.print("Welcome");
+		System.out.println("JAVA");
+		System.out.print("Selenium");
+		System.out.print("QA Session");
+		System.out.println("Programming Language");
+		System.out.print("Chrome");
+		System.out.println("Firefox");
+		
+	}
+
+}
